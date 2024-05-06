fa2cbb1a8fff08c45970a04675228e2c4be59be0
luq <luqdek@gmail.com>
2024-05-06 23:40:21 +0100

    Change error message again

--- src/main/java/com/todoAPI/demo/todoItem/TodoItemController.java
+++ src/main/java/com/todoAPI/demo/todoItem/TodoItemController.java
@@ -27,7 +27,7 @@ public class TodoItemController {
         todoItem.setMessage(message);
         Optional<User> user = userRepository.findById(userId);
         if(user.isEmpty()){
-            return "Error";
+            return "Errorz";
         }
         todoItem.setUser(user.get());
         todoItemRepository.save(todoItem);
